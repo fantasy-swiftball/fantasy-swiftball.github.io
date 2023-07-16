6a5fc6a1d32ab259a27ead93f0d7c9a56f34e4ec
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-07-16 07:48:56 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -8,11 +8,11 @@ var repanswer = blackandredsnake; // blackandredsnake or newrepjumpsuit
 var snanswer = champagnegown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or newsngown
 var shirt22answer = WTSAEshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
 var folkloreanswer = creamfolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or newfolkloredress
-var set1989answer = ; // green1989 or orange1989 or pink1989 or new1989set
-var guitarspeechanswer = ; // speechguitaryes or speechguitarno
-var guitaralbumanswer = ; // guitar + album name
-var pianospeechanswer = ; // speechpianoyes or speechpianono
-var pianoalbumanswer = ; // piano + album name
+var set1989answer = pink1989; // green1989 or orange1989 or pink1989 or new1989set
+var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
+var guitaralbumanswer = guitarred; // guitar + album name
+var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
+var pianoalbumanswer = pianosn; // piano + album name
 var midnightsanswer = ; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
 var mranswer = ; // mrnavy or mrblurple or newmrbodysuit
 var karmaanswer = ; // karmalightpink or or karmamulticolor or karmamagenta or nokarmajacket or newkarmajacket
@@ -21,5 +21,5 @@ var unhingedanswer = ; // unhingedyes or unhingedno
 var mishapanswer = ; // mishapyes or mishapno
 
 
-const guitaranswer = ;
-const pianoanswer = ;
+const guitaranswer = ['Starlight', 'starlight'];
+const pianoanswer = ['Back To December', 'back to december', 'Back to december', 'BTD'];
