81cc6e4cc411d76f252adcf307785cc5be510fb7
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-07-16 08:12:46 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -13,12 +13,12 @@ var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
 var guitaralbumanswer = guitarred; // guitar + album name
 var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
 var pianoalbumanswer = pianosn; // piano + album name
-var midnightsanswer = ; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
-var mranswer = ; // mrnavy or mrblurple or newmrbodysuit
-var karmaanswer = ; // karmalightpink or or karmamulticolor or karmamagenta or nokarmajacket or newkarmajacket
-var guestanswer = ; // guestyes or guestno
-var unhingedanswer = ; // unhingedyes or unhingedno
-var mishapanswer = ; // mishapyes or mishapno
+var midnightsanswer = silversequinmidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
+var mranswer = mrblurple; // mrnavy or mrblurple or newmrbodysuit
+var karmaanswer = karmamagenta; // karmalightpink or or karmamulticolor or karmamagenta or nokarmajacket or newkarmajacket
+var guestanswer = guestno; // guestyes or guestno
+var unhingedanswer = unhingedno; // unhingedyes or unhingedno
+var mishapanswer = mishapno; // mishapyes or mishapno
 
 
 const guitaranswer = ['Starlight', 'starlight'];
