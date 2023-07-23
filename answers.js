6c4b2a2528760d4cedbf6e17a3e7b95239f67a3c
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-07-23 10:21:02 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,8 +1,8 @@
 var loveranswer = pinkandblue; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
 var themananswer = silvertheman; // blacktheman or silvertheman or newtheman
 var loverguitaranswer = pinkloverguitar; //pinkloverguitar or blueloverguitar
-var fearlessanswer = fringedress; // fringedress or goldnoodle or silvernoodle or newfearlessdress
-var evermoreanswer = orangewithflowers; // orangewithflowers or bronzeevermore or newevermoredress
+var fearlessanswer = silvernoodle; // fringedress or goldnoodle or silvernoodle or newfearlessdress
+var evermoreanswer = bronzedress; // orangewithflowers or bronzeevermore or newevermoredress
 var cpcheeranswer = cpless2; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
 var repanswer = blackandredsnake; // blackandredsnake or newrepjumpsuit
 var snanswer = champagnegown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or newsngown
@@ -10,16 +10,16 @@ var shirt22answer = WTSAEshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or
 var folkloreanswer = creamfolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or newfolkloredress
 var set1989answer = pink1989; // green1989 or orange1989 or pink1989 or new1989set
 var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
-var guitaralbumanswer = guitarred; // guitar + album name
-var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
-var pianoalbumanswer = pianosn; // piano + album name
-var midnightsanswer = silversequinmidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
+var guitaralbumanswer = guitarrep; // guitar + album name
+var pianospeechanswer = speechpianoyes; // speechpianoyes or speechpianono
+var pianoalbumanswer = pianored; // piano + album name
+var midnightsanswer = combomidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
 var mranswer = mrblurple; // mrnavy or mrblurple or newmrbodysuit
-var karmaanswer = karmamulticolor; // karmalightpink or or karmamulticolor or karmamagenta or nokarmajacket or newkarmajacket
-var guestanswer = guestno; // guestyes or guestno
-var unhingedanswer = unhingedno; // unhingedyes or unhingedno
+var karmaanswer = karmamagenta; // karmalightpink or or karmamulticolor or karmamagenta or nokarmajacket or newkarmajacket
+var guestanswer = guestyes; // guestyes or guestno
+var unhingedanswer = unhingedyes; // unhingedyes or unhingedno
 var mishapanswer = mishapno; // mishapyes or mishapno
 
 
-const guitaranswer = ['Starlight', 'starlight'];
-const pianoanswer = ['Back To December', 'back to december', 'Back to december', 'BTD'];
+const guitaranswer = ['TIWWCHNT', 'This is why we can't have nice things'];
+const pianoanswer = ['EHC'];
