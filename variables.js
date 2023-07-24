5a18b260acc3d4593f94d9609f7566bf638b4fdc
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-07-24 10:53:25 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-v mesages and points
+// mesages and points
 var lovercorr = document.getElementById("lover-correct");
 var loverincorr = document.getElementById("lover-incorrect");
 var loverright = document.getElementById("lover-right");
