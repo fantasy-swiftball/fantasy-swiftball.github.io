ba2514d7898a8da0e8e6815ae19480acab929782
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-08-09 13:18:55 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,32 +1,32 @@
-var loveranswer = blueandgold; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
-var themananswer = blacktheman; // blacktheman or silvertheman or newtheman
-var loverguitaranswer = blueloverguitar; //pinkloverguitar or blueloverguitar
-var fearlessanswer = silvernoodle; // fringedress or goldnoodle or silvernoodle or newfearlessdress
-var evermoreanswer = bronzeevermore; // orangewithflowers or bronzeevermore or newevermoredress
-var cpcheeranswer = cpless3; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
+var loveranswer = purplewithtassels; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
+var themananswer = newtheman; // blacktheman or silvertheman or newtheman
+var loverguitaranswer = pinkloverguitar; //pinkloverguitar or blueloverguitar
+var fearlessanswer = goldnoodle; // fringedress or goldnoodle or silvernoodle or newfearlessdress
+var evermoreanswer = orangewithflowers; // orangewithflowers or bronzeevermore or newevermoredress
+var cpcheeranswer = cprecord; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
 var repanswer = blackandredsnake; // blackandredsnake or newrepjumpsuit
-var snanswer = champagnegown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or newsngown
-var shirt22answer = WTSAEshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
-var folkloreanswer = purplefolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or newfolkloredress
-var set1989answer = orange1989; // green1989 or orange1989 or pink1989 or new1989set
+var snanswer = cupcakegown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or newsngown
+var shirt22answer = WANEGBTshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
+var folkloreanswer = greenfolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or newfolkloredress
+var set1989answer = green1989; // green1989 or orange1989 or pink1989 or new1989set
 var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
-var guitaralbumanswer = guitarrep; // guitar + album name
+var guitaralbumanswer = guitar1989; // guitar + album name
 var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
-var pianoalbumanswer = pianofolklore; // piano + album name
-var midnightsanswer = purplemidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
-var mranswer = mrnavy; // mrnavy or mrblurple or newmrbodysuit
-var karmaanswer = karmamagenta; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
+var pianoalbumanswer = pianorep; // piano + album name
+var midnightsanswer = silversequinmidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
+var mranswer = newmrbodysuit; // mrnavy or mrblurple or newmrbodysuit
+var karmaanswer = karmablue; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
 var haimanswer = haimyes; // haimyes or haimno
 var guestanswer = guestno; // guestyes or guestno
 var unhingedanswer = unhingedno; // unhingedyes or unhingedno
-var mishapanswer = mishapyes; // mishapyes or mishapno
+var mishapanswer = mishapno; // mishapyes or mishapno
 
 
-var guitarsonganswer = "Dress";
-var pianosonganswer = "Exile";
+var guitarsonganswer = "I Know Places";
+var pianosonganswer = "King Of My Heart";
 var surpriseanswers = guitarsonganswer + " " + "(Guitar)" + " " + "/" + " " +  pianosonganswer + " " + "(Piano)";
 
-const guitaranswer = [guitarsonganswer, 'dress'];
-const pianoanswer = [pianosonganswer, 'exile']; 
+const guitaranswer = [guitarsonganswer, 'IKP'];
+const pianoanswer = [pianosonganswer, 'KOMH']; 
 
-alert("Ballot calculation is now updated for the show on 8/7");
+alert("Ballot calculation is now updated for the show on 8/8");
