cd2712a3c08f7d956c0f396f5495417066a305d3
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-08-24 21:54:59 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,32 +1,30 @@
-var loveranswer = pinkandblue; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
-var themananswer = silvertheman; // blacktheman or silvertheman or purpletheman or newtheman
-var loverguitaranswer = pinkloverguitar; //pinkloverguitar or blueloverguitar
-var fearlessanswer = fringedress; // fringedress or goldnoodle or silvernoodle or newfearlessdress
-var evermoreanswer = orangewithflowers; // orangewithflowers or bronzeevermore or newevermoredress
-var cpcheeranswer = cpmore3; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
-var repanswer = blackandredsnake; // blackandredsnake or newrepjumpsuit
-var snanswer = newsngown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or newsngown
-var shirt22answer = ALGOATMshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
-var folkloreanswer = newfolkloredress; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or newfolkloredress
-var set1989answer = new1989set; // green1989 or orange1989 or pink1989 or new1989set
-var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
-var guitaralbumanswer = guitar1989; // guitar + album name
-var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
-var pianoalbumanswer = pianorep; // piano + album name
-var midnightsanswer = lightbluemidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
-var mranswer = mrrorschach; // mrnavy or mrblurple or mrrorschach or newmrbodysuit
-var karmaanswer = karmablue; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
-var haimanswer = haimyes; // haimyes or haimno
-var guestanswer = guestno; // guestyes or guestno
-var unhingedanswer = unhingedyes; // unhingedyes or unhingedno
-var mishapanswer = mishapyes; // mishapyes or mishapno
+var loveranswer = ; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
+var themananswer = ; // blacktheman or silvertheman or purpletheman or newtheman
+var loverguitaranswer = ; //pinkloverguitar or blueloverguitar
+var fearlessanswer = ; // fringedress or goldnoodle or silvernoodle or newfearlessdress
+var evermoreanswer = ; // orangewithflowers or bronzeevermore or newevermoredress
+var cpcheeranswer = ; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
+var repanswer = ; // blackandredsnake or newrepjumpsuit
+var snanswer = ; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or elsagown or newsngown
+var shirt22answer = ; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
+var folkloreanswer = ; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or bluefolklore or newfolkloredress
+var set1989answer = ; // green1989 or orange1989 or pink1989 or blue1989 or new1989set
+var guitarspeechanswer = ; // speechguitaryes or speechguitarno
+var guitaralbumanswer = ; // guitar + album name
+var pianospeechanswer = ; // speechpianoyes or speechpianono
+var pianoalbumanswer = ; // piano + album name
+var midnightsanswer = ; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
+var mranswer = ; // mrnavy or mrblurple or mrrorschach or newmrbodysuit
+var karmaanswer = ; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
+var haimanswer = ; // haimyes or haimno
+var guestanswer = ; // guestyes or guestno
+var unhingedanswer = ; // unhingedyes or unhingedno
+var mishapanswer = ; // mishapyes or mishapno
 
 
-var guitarsonganswer = "New Romantics";
-var pianosonganswer = "New Years Day";
+var guitarsonganswer = "";
+var pianosonganswer = "";
 var surpriseanswers = guitarsonganswer + " " + "(Guitar)" + " " + "/" + " " + pianosonganswer + " " + "(Piano)";
 
-const guitaranswer = [guitarsonganswer, 'new romantics'];
-const pianoanswer = [pianosonganswer, 'NYD']; 
-
-alert("Updated for 8/9 show");
+const guitaranswer = [guitarsonganswer, ''];
+const pianoanswer = [pianosonganswer, '']; 
