3a1d962afe73973d042c4263969168e923df0a61
fantasy-swiftball <138615381+fantasy-swiftball@users.noreply.github.com>
2023-08-27 16:30:35 +0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -1,29 +1,29 @@
-var loveranswer = blueandgold; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
-var themananswer = blacktheman; // blacktheman or silvertheman or purpletheman or newtheman
-var loverguitaranswer = blueloverguitar; //pinkloverguitar or blueloverguitar
-var fearlessanswer = silvernoodle; // fringedress or goldnoodle or silvernoodle or newfearlessdress
-var evermoreanswer = bronzeevermore; // orangewithflowers or bronzeevermore or newevermoredress
-var cpcheeranswer = cpless2; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
+var loveranswer = purplewithtassels; // pinkandblue or blueandgold or purplewithtassels or newloverbodysuit
+var themananswer = purpletheman; // blacktheman or silvertheman or purpletheman or newtheman
+var loverguitaranswer = pinkloverguitar; //pinkloverguitar or blueloverguitar
+var fearlessanswer = goldnoodle; // fringedress or goldnoodle or silvernoodle or newfearlessdress
+var evermoreanswer = orangewithflowers; // orangewithflowers or bronzeevermore or newevermoredress
+var cpcheeranswer = cpless3; // cpless1 or cpless2 or cpless3 or cpmore3 or cprecord
 var repanswer = blackandredsnake; // blackandredsnake or newrepjumpsuit
-var snanswer = elsagown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or elsagown or newsngown
-var shirt22answer = WTSAEshirt; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
-var folkloreanswer = greenfolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or bluefolklore or newfolkloredress
-var set1989answer = pink1989; // green1989 or orange1989 or pink1989 or blue1989 or new1989set
+var snanswer = champagnegown; // pinkballgown or champagnegown or tissuegown or silvergown or cupcakegown or elsagown or newsngown
+var shirt22answer = WANEGBT; // WANEGBTshirt or WTSAEshirt or ALGOATMshirt or new22shirt
+var folkloreanswer = bluefolklore; // purplefolklore or creamfolklore or pinkfolklore or greenfolklore or bluefolklore or newfolkloredress
+var set1989answer = green1989; // green1989 or orange1989 or pink1989 or blue1989 or new1989set
 var guitarspeechanswer = speechguitaryes; // speechguitaryes or speechguitarno
-var guitaralbumanswer = guitarfearless; // guitar + album name
+var guitaralbumanswer = guitarlover; // guitar + album name
 var pianospeechanswer = speechpianono; // speechpianoyes or speechpianono
 var pianoalbumanswer = pianomidnights; // piano + album name
 var midnightsanswer = purplemidnights; // lightbluemidnights or pinkmidnights or purplemidnights or silversequinmidnights or combomidnights or newmidnightsdress 
-var mranswer = mrrorschach; // mrnavy or mrblurple or mrrorschach or newmrbodysuit
-var karmaanswer = karmamagenta; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
+var mranswer = mrnavy; // mrnavy or mrblurple or mrrorschach or newmrbodysuit
+var karmaanswer = karmamulticolor; // karmalightpink or or karmamulticolor or karmamagenta or karmablue or nokarmajacket or newkarmajacket
 var guestanswer = guestno; // guestyes or guestno
 var unhingedanswer = unhingedno; // unhingedyes or unhingedno
 var mishapanswer = mishapno; // mishapyes or mishapno
 
 
-var guitarsonganswer = "Tell Me Why";
-var pianosonganswer = "Snow On The Beach";
+var guitarsonganswer = "Cornelia Street";
+var pianosonganswer = "Youre On Your Own Kid";
 var surpriseanswers = guitarsonganswer + " " + "(Guitar)" + " " + "/" + " " + pianosonganswer + " " + "(Piano)";
 
-const guitaranswer = [guitarsonganswer, 'TMW'];
-const pianoanswer = [pianosonganswer, 'SOTB']; 
+const guitaranswer = [guitarsonganswer, 'cornelia street'];
+const pianoanswer = [pianosonganswer, 'YOYOK']; 
